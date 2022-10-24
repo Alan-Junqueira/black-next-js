12192c3b90633d5e1bc4f598caa9ac85ca8d1ad4
Alan Junqueira <contato.alanjunqueira@gmail.com>
2022-10-24 17:01:57 -0300

    first commit

---
+++ pages/cart.tsx
@@ -0,0 +1,35 @@
+// pages/cart.tsx
+
+import { NextPage } from "next";
+import Head from "next/head";
+import Header from "../src/components/Header";
+import {Container} from 'reactstrap'
+import CartTable from "../src/components/CartTable";
+import CartTotal from "../src/components/CartTotal";
+
+const Cart: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">
+            Carrinho
+          </h1>
+
+          <CartTable />
+          <CartTotal />
+        </Container>
+      </main>
+    </>
+  )
+}
+
+export default Cart
